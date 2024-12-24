1f4a0dc8cd31006324544ced4f1e50abcfc2ca0d
pavelica-agile <75150739+pavelica-agile@users.noreply.github.com>
2024-12-24 12:01:34 -0800

    Added doxygen comments to events.py

--- events.py
+++ events.py
@@ -1,15 +1,33 @@
+"""
+@file events.py
+@brief This file contains the implementation of generic UI event structures.
+"""
+
 # Generic UI events structure
 
 class EventSource(object):
-    """Generate GUI events.
+    """
+    @brief Generate GUI events.
     """
     _events = {}
 
     @classmethod
     def bind(cls, event, handler):
+        """
+        @brief Bind an event to a handler.
+
+        @param event The event to bind.
+        @param handler The handler function to call when the event is emitted.
+        """
         cls._events.setdefault(cls, {}).setdefault(event, []).append(handler)
 
     def emit(self, event, **data):
+        """
+        @brief Emit an event and call all bound handlers.
+
+        @param event The event to emit.
+        @param data Additional data to pass to the handler functions.
+        """
         try:
             for handler in self._events[self.__class__][event]:
                 handler(self, **data)
